f5e393d0adfef9fe275de359b44cd4b5ee7d81d8
brnbarbosa <bruno.ebarbosa@gmail.com>
2025-04-28 11:58:47 -0300

    Trying to understand typer commands

--- task_tracker.py
+++ task_tracker.py
@@ -8,26 +8,40 @@ app = typer.Typer()
 
 table = Table("Name", "Item")
 
-@app.command()
+class Player:
+    def __init__(self, name, gun):
+        self.name = name
+        self.gun = gun
+
+a = Player("Bruno", "38")
+b = Player("Carlos", "22")
+
+players : list = [a, b]
+
+@app.command("init")
 def main(
-        name: Annotated[str, typer.Option("--name", "-n", help="Your name", show_default=False)],
+        name: Annotated[str, typer.Argument(help="Your name", rich_help_panel="Player name", show_default=False)],
         gun: Annotated[str, typer.Argument(help="Your Gun", rich_help_panel="Weapons")] =  "", 
         formal: Annotated[bool, typer.Option(help="Formal or Informal greeting")]= False,
         lastname: Annotated[str, typer.Option(help="Last Name", prompt=True, show_default=False)]="Barbosa"):
-    """
-    Showed only in Help.
-    """
+
     if formal:
         print(f"[red]Good day Ms.[/red] [bold green]{name} {lastname} {gun}[/bold green]")
     else:
         print(f"Testing {name} {lastname} {gun}")
-        add(name, gun)
-    
 
-
+@app.command()
 def add(name: str, gun: str):
-    table.add_row(name, gun)
+    new = Player(name, gun)
+    players.append(new)
+
+
+@app.command("print")
+def print_table():
+    for p in players:
+        table.add_row(p.name, p.gun)
     print(table)
+
 
 if __name__ == "__main__":
     app()
