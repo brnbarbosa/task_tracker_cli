9d164004c3ce5ef35f6de871eee321b422491b3a
brnbarbosa <bruno.ebarbosa@gmail.com>
2025-05-08 11:21:44 -0300

    Commands done and delete created

--- task_tracker.py
+++ task_tracker.py
@@ -18,15 +18,25 @@ table = Table("Description", "DeadLine", "Status")
 console = Console()
 
 class Task:
-    def __init__(self, description: str, date: datetime):
+    def __init__(self, description: str, deadline: datetime, status: str = "default"):
         self.description: str = description
-        self.date: datetime = date
+        self.deadline: datetime = deadline
+        self.status: str = self.check_status()
 
     def to_dict(self) -> dict:
-        return {"description": self.description, "date": str(self.date)}
+        return {"description": self.description, "deadline": str(self.deadline), "status" : self.status }
+    
+    def check_status(self) -> str:
+        today = datetime.today()
+        if self.deadline >= today:
+            return 'To do'
+        elif self.deadline < today:
+            return 'Delayed'
+        else:
+            return 'Waiting definition'
 
 
-@app.command("init")
+@app.command("init", help="Init the database")
 def main():
     if os.path.exists(FILE_PATH):
         print("File exists")
@@ -35,9 +45,9 @@ def main():
         with open(FILE_PATH, 'w') as write_file:
             json.dump(tasks, write_file)
 
-@app.command('add')
-def add(task: Annotated[str, typer.Argument(help="Enter the task description")], date: Annotated[datetime, typer.Option(help="Enter the deadline for this task", prompt="Enter the deadline: ")]):
-    new = Task(task, date).to_dict()
+@app.command('add', help="Add a new task, insert the task description and the deadline will be prompt")
+def add(task: Annotated[str, typer.Argument(help="Enter the task description")], deadline: Annotated[datetime, typer.Option(help="Enter the deadline for this task", prompt="Enter the deadline")]):
+    new = Task(task, deadline).to_dict()
     task_list = []
 
     try:
@@ -58,14 +68,49 @@ def add(task: Annotated[str, typer.Argument(help="Enter the task description")],
         json.dump(task_list, json_file, indent=4)
 
 
-@app.command("print")
+@app.command("print", help="Print task table")
 def print_table():
     with open(FILE_PATH) as json_file:
         data = json.load(json_file)
         for i in range(len(data)):
-            table.add_row(data[i]['description'], data[i]['date'][:10])
+            table.add_row(data[i]['description'], data[i]['deadline'][:10], data[i]['status'])
 
     print(table)
+
+@app.command("done", help="Set the task status as done")
+def done(task: Annotated[str, typer.Argument(help="Enter task description")]):
+    temp_data = {}
+
+    with open(FILE_PATH, 'r') as read_file:
+        temp_data = json.load(read_file)
+        
+    with open(FILE_PATH, 'w') as json_file:
+        for i in range(len(temp_data)):
+            if temp_data[i]['description'] == task:
+                temp_data[i]['status'] = 'Done'
+        json.dump(temp_data, json_file, indent=4)
+
+@app.command("delete", help="Delete a specific task")
+def delete(task: Annotated[str, typer.Argument(help="Enter task description")]):
+    temp_data = []
+    removed_value = {}
+    try:
+        with open(FILE_PATH, 'r') as read_file:
+            temp_data = json.load(read_file)
+    except:
+        print('file inexist')
+
+    try:
+        with open(FILE_PATH, 'w') as json_file:
+            for i in range(len(temp_data)):
+                if temp_data[i]['description'] == task:
+                    removed_value = temp_data.pop(i)
+
+            json.dump(temp_data, json_file, indent=4)
+    except:
+        print(removed_value)
+
+    print(f'{removed_value['description']} was sucessfull removed')
 
 if __name__ == "__main__":
     app()
